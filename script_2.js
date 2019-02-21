942abe30da4ad86701ffa8c7fa1cfaafd9fca251
Noé Marguillard <noe.marguillard@essec.edu>
2019-02-21 13:08:21 +0100

    2 fait

---
+++ script_2.js
@@ -0,0 +1,12 @@
+number = prompt('De quel nombre veux-tu calculer la factorielle ?');
+
+function factorial(number) 
+{ 
+  if (number === 0)
+ {
+    return 1;
+ }
+  return number * factorial(number-1);      
+}
+
+console.log(factorial(number));
